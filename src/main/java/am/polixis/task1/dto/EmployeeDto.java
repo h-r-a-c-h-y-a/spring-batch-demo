3befcb4a5c123f11f18e59a67aa0781b80a012ae
Hrachya Khachatryan <hro070890@gmail.com>
2021-03-13 11:29:35 +0400

    created a data transfer object and model for manipulation with data

---
+++ src/main/java/am/polixis/task1/dto/EmployeeDto.java
@@ -0,0 +1,15 @@
+package am.polixis.task1.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmployeeDto {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String date;
+}
---
+++ src/main/java/am/polixis/task1/entity/Employee.java
@@ -0,0 +1,29 @@
+package am.polixis.task1.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    private LocalDate date;
+}
