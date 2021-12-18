9114586f8058a4b974a79f4f0d388320e9470d0c
Hrachya Khachatryan <hrachya_khachatryan@epam.com>
2021-12-18 21:24:16 +0400

    removed unused import

--- src/main/java/am/polixis/spring_batch_demo/BatchDemoApplication.java
+++ src/main/java/am/polixis/spring_batch_demo/BatchDemoApplication.java
@@ -3,8 +3,6 @@ package am.polixis.spring_batch_demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.PreDestroy;
-
 @SpringBootApplication
 public class BatchDemoApplication {
 
