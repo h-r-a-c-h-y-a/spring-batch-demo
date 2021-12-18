f2a97e5511c4e72ccd35bf8af128350a3e18d02c
Hrachya Khachatryan <hrachya_khachatryan@epam.com>
2021-12-18 20:51:14 +0400

    added piece of code in unit test which based on sys env variable might fail test

--- src/test/java/am/polixis/spring_batch_demo/BatchDemoApplicationTests.java
+++ src/test/java/am/polixis/spring_batch_demo/BatchDemoApplicationTests.java
@@ -3,11 +3,15 @@ package am.polixis.spring_batch_demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class BatchDemoApplicationTests {
 
     @Test
     void contextLoads() {
+        String env = System.getenv("env");
+        assertEquals("PROD", env);
     }
 
 }
