2c40f8f47202db6c754a52021c6abe7d3d793ce9
Hrachya Khachatryan <hrachya_khachatryan@epam.com>
2021-12-19 01:08:22 +0400

    changed test

--- src/test/java/am/polixis/spring_batch_demo/BatchDemoApplicationTests.java
+++ src/test/java/am/polixis/spring_batch_demo/BatchDemoApplicationTests.java
@@ -2,12 +2,19 @@ package am.polixis.spring_batch_demo;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class BatchDemoApplicationTests {
 
     @Test
     void test() {
+        String env = System.getenv("env");
+        assertEquals("DEV", env);
+    }
+
+    @Test
+    void test2() {
         String env = System.getenv("env");
         assertNotEquals("DEV", env);
     }
